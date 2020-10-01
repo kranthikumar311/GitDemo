9efa7ebdc311df08b3e3492c9479e7da80889037
kranthikumar311 <kranthikumarchennoji@gmail.com>
2020-10-01 13:13:13 +0530

    Commit from the Different Time ZONE

--- FirstDemo.py
+++ FirstDemo.py
@@ -1,4 +1,4 @@
-print("hello")
+print("hello world ")
 # here are the comments i have defined
 
 a = 3
@@ -7,7 +7,7 @@ print(a)
 Str = "Hello World"
 print(Str)
 
-b, c, d = 5, 6.4, "Great"
+b, c, d = 5, 6.4, "Great KIng of the Osaris"
 
 #print("Value is"+b+"fdfd")
 

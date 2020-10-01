fe44121e3c1c8ebf281986b0a24ee72486752ab5
kranthikumar311 <kranthikumarchennoji@gmail.com>
2020-10-02 01:14:05 +0530

    Architect original guy changes

--- ChildImp.py
+++ ChildImp.py
@@ -3,6 +3,9 @@ from pythonBasics.OopsDemo import Calculator
 
 class ChildImpl(Calculator):
     num2 = 2000
+    num3 = 3000
+    num4 = 4000
+    num5 = 5000
 
     def __init__(self):
         Calculator.__init__(self, 2, 10)
